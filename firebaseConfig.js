9d5a4427972bbe51f0559eaf93b3aff8f8003313
Youngjnick <youngjnick@gmail.com>
2025-05-23 21:36:28 -0400

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,8 +1,9 @@
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyBUJaf1a-_CjrbPXe48YOVbp1cN9UwcWB8",
   authDomain: "massage-therapy-smart-study.firebaseapp.com",
   projectId: "massage-therapy-smart-study",
-  storageBucket: "massage-therapy-smart-study.firebasestorage.app",
+  storageBucket: "massage-therapy-smart-study.appspot.com",
   messagingSenderId: "390700279128",
   appId: "1:390700279128:web:1d9fe9d99dca4a9302b12d",
   measurementId: "G-C2E27FGQ6X"
+};
