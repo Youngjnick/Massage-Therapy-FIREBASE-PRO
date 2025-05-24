52a156aec113bd70056f56d9279d55e829d2f8c6
Youngjnick <youngjnick@gmail.com>
2025-05-23 21:25:16 -0400

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyARFhLgGr73voN2rZRCycMtup_VKdy2s18",
+  authDomain: "massage-therapy-smart-study.firebaseapp.com",
+  projectId: "massage-therapy-smart-study",
+  storageBucket: "massage-therapy-smart-study.appspot.com",
+  messagingSenderId: "390700279128",
+  appId: "1:390700279128:web:1d9fe9d99dca4a9302b12d",
+  measurementId: "G-C2E27FGQ6X"
+};
