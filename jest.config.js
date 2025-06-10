624d0c61e6c00c7ced07370a1d2c4ea4e5253641
Tim Williams <macuser@iMac.fios-router.home>
2025-06-10 06:07:30 -0400

    Remove all files from gh-pages

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  roots: ['<rootDir>/src', '<rootDir>/tests'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-};
--- src/questions/index.ts
+++
@@ -1,8 +0,0 @@
-import { db } from '../firebase/firebaseConfig';
-import { collectionGroup, getDocs } from 'firebase/firestore';
-import { Question } from '../types';
-
-export const getQuestions = async (): Promise<Question[]> => {
-  const snapshot = await getDocs(collectionGroup(db, 'questions'));
-  return snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() } as Question));
-};
