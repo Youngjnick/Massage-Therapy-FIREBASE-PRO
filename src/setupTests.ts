350014183caee07e53df7d6051c58c26ce34c752
Jason Young <youngjnick@gmail.com>
2025-06-13 14:38:12 -0400

    Deploy fresh build and static files to gh-pages

--- src/setupTests.ts
+++
@@ -1,8 +0,0 @@
-import { TextEncoder, TextDecoder } from 'util';
-if (typeof global.TextEncoder === 'undefined') {
-  // @ts-expect-error: Assigning Node.js TextEncoder to global for Jest
-  global.TextEncoder = TextEncoder;
-  // @ts-expect-error: Assigning Node.js TextDecoder to global for Jest
-  global.TextDecoder = TextDecoder;
-}
-import '@testing-library/jest-dom';
