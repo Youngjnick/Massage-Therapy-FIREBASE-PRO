582064905fd9033b515f6cf759336b4f4ea4c128
Jason Young <youngjnick@gmail.com>
2025-06-22 12:25:03 -0400

    chore: add start-server-and-test for Playwright/dev server sync
    - Added start-server-and-test to dev dependencies in install-deps.sh and package.json
    - Added 'test:e2e' script to package.json for running dev server and Playwright tests in sync
    - Improves reliability of e2e tests by ensuring server is running before tests start
    - Updated install-deps.sh to always install all required tools for local and CI environments
    
    Developers can now use 'npm run test:e2e' for seamless e2e testing

--- install-deps.sh
+++ install-deps.sh
@@ -6,7 +6,7 @@ echo "Installing all project dependencies..."
 npm install
 
 echo "Installing required dev dependencies for testing, linting, and type support..."
-npm install --save-dev react @types/react @testing-library/react @types/testing-library__react jest @types/jest eslint @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint-plugin-react eslint-config-prettier @babel/core @babel/eslint-parser @babel/preset-env @babel/preset-react @babel/preset-typescript @vitejs/plugin-react vite typescript @types/react-dom @types/node babel-jest babel-plugin-transform-import-meta babel-plugin-transform-vite-meta-env @playwright/test @testing-library/jest-dom @testing-library/user-event identity-obj-proxy jest-environment-jsdom gh-pages cross-fetch firebase-admin react-icons
+npm install --save-dev react @types/react @testing-library/react @types/testing-library__react jest @types/jest eslint @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint-plugin-react eslint-config-prettier @babel/core @babel/eslint-parser @babel/preset-env @babel/preset-react @babel/preset-typescript @vitejs/plugin-react vite typescript @types/react-dom @types/node babel-jest babel-plugin-transform-import-meta babel-plugin-transform-vite-meta-env @playwright/test @testing-library/jest-dom @testing-library/user-event identity-obj-proxy jest-environment-jsdom gh-pages cross-fetch firebase-admin react-icons start-server-and-test
 
 echo "Installing required dependencies..."
 npm install firebase glob json5 jspdf react-dom react-router-dom strip-json-comments
